9827de2c80da221f26a1c88ab7827342d89d6bb0
Kondratyev Evgeniy <evgeniy.kondratyev@danfoss.com>
2019-01-10 16:52:38 +0300

    nav edited

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,29 @@
 var humb=document.querySelector(".hamburger");
 var menu=document.querySelector("nav");
 var wrap=document.querySelector(".wrap");
-humb.onclick=function(){
-    this.classList.toggle("is-active");
+function moveNav(){
+    humb.classList.toggle("is-active");
     wrap.classList.toggle("wrap-slided");
     menu.classList.toggle("nav-activeted");
 }
+
+wrap.onclick=function(e){
+    if(this.getAttribute("class")=="wrap wrap-slided"){
+        humb.onclick=moveNav();
+    }else{
+        humb.onclick=moveNav;
+    }
+    
+    /*if(menu.getAttribute("class")=="nav-activeted"){
+        humb.onclick=moveNav();
+    }*/ 
+    console.log(this);
+}
+/*wrap.addEventListener("click",function(e){ 
+    if(menu.getAttribute("class")=="nav-activeted"){
+        moveNav();
+    }
+});*/
 
 $('.post').slick({
     dots: true,
